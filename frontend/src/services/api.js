9e2fa32b65f33673d5499cfcc309a183ec6d4a60
tribebharat <abhiesttechnologies@gmail.com>
2025-12-05 01:31:56 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE_URL = 'http://localhost:8000/api'
+export const API_BASE_URL = 'https://tribebharat.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
